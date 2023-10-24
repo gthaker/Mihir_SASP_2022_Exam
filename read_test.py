f3b1e166c21d793d6417e14dbe15d8e76eeb1c15
Gautam Thaker <ghthaker@gmail.com>
2023-10-24 19:25:31 -0400

    WIP, almost done, just the last question's end is not detected as we don't have question=149.

--- read_test.py
+++ read_test.py
@@ -7,8 +7,9 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('-s', '--skip', help="skip this question number", type=int, nargs='*')
 parser.add_argument('-i', '--input', help='test text input file', required=True)
+parser.add_argument('-a', '--answers', help='file containing test answer', required=True)
 parser.add_argument('-d', '--debug', help='debug level. an int, default=0', type=int, default=0, required=False)
-parser.add_argument('-y', '--year', help='which year input we are processing. An int.', type=int, required=True)
+parser.add_argument('-y', '--year', help='which year input we are processing. An int.', type=int, required=False)
 args = parser.parse_args()
 
 if args.skip == None:
@@ -54,7 +55,7 @@ def trim_newlines_at_end(ans):
         return ans
     return ans[:-count]
 
-def get_block(starting_re, nre, prev_line_blank=False):   # nre=next_section_re
+def get_block(starting_re, nre, prev_line_blank=False ):   # nre=next_section_re
     global p
     ans = []
     while p < nlines:
@@ -78,19 +79,14 @@ discarded_ques = []
 all_ques = []
 
 # when we are reading the answer options, this dict tells us the next_section_regex
-opt_nre_vals = {'A': r'B\.? ', 'B': r'C\.? ', 'C': r'D\.? ', 'D': r'^\s*$',
-                'a': r'b\.? ', 'b': r'c\.? ', 'c': r'd\.? ', 'd': r'^\s*$',
+opt_nre_vals = {'A': r'(?i)B\. ', 'B': r'(?i)C\. ', 'C': r'(?i)D\. ', 'D': r'(?i)E\.', 'E': r'^\d+\. ',
                 }
 
 while p < nlines:
     log('===========================================================================')
     nre = r'^A\. '
-    if args.year == 2018:
-        nre = r'^a\. '
-    if args.year == 2017 or args.year == 2016:
-        nre = r'^\s*\(A\) '
 
-    q = get_block(r'^\s?%s\. ' % nextq, nre, prev_line_blank=True)   # find block starting with "1. xxx"  "2. yyy" etc
+    q = get_block(r'^\s?%s\. ' % nextq, nre, prev_line_blank=False)   # find block starting with "1. xxx"  "2. yyy" etc
     if q == None:
         log(f'question {nextq=} not read, existing loop.')
         break
@@ -105,78 +101,14 @@ while p < nlines:
     log(' ==', qnum, q)
 
     opts = []
-    opt_list = 'ABCD'
-    if args.year == 2018:
-        opt_list = 'abcd'
+    opt_list = 'ABCDE'
 
     for opt in opt_list:
-        if args.year == 2017 or args.year == 2016:
-            nre = r'^\s*\(%s\) ' % chr( ord(opt)+1 )
-            if opt == 'D': nre = r'^Key: '
-            opts.append(get_block(r'^\s*\(%s\) ' % opt, nre))
-        else:
-            opts.append(get_block(r'^\s*%s\.? ' % opt, opt_nre_vals[opt]))
+        opts.append(get_block(r'^\s*(?i)%s\.? ' % opt, opt_nre_vals[opt]))
     log(f'{opts=}')
 
-    if args.year == 2021:
-        key = get_block(r'^Key:?', r'^Domain: ')  # may come back as 1 line or multiple lines
-    elif args.year == 2020 or args.year == 2018:
-        key = get_block(r'^Key:?', r'^Rationales? ?: ')  # may come back as 1 line or multiple lines
-    elif args.year == 2019:
-        key = get_block(r'^Key:?', r'^Citations? ?: ?')  # may come back as 1 line or multiple lines
-    elif args.year == 2017 or args.year == 2016:
-        key = get_block(r'^Key:?', r'^Solution: ?')  # may come back as 1 line or multiple lines
-    log(f'block  {key=}')
-    mkey = re.match(r'^Key:? ?(This item was 0-weighted|\w)(?: & (\w))?', key[0])
-    if mkey: log(f'1: {mkey.group(1,2)=}')
-    if not mkey and len(key) > 1:  # Key block was multiple lines in the input
-        mkey = re.match(r'^\s*(?:(This item was 0-weighted|\w)(?: & (\w))?)', key[1])
-        log(f'2: {mkey.group(1,2)=}')
-    if mkey:
-        key = [mkey.group(1),]
-        if mkey.group(2):
-            key.append(mkey.group(2))
-    else:
-        print('can not process key (%s)' % key)
-        sys.exit(1)
-    if key[0].startswith('This item was 0-weighted'):
-        keep_question = False  # but we will still need to extract following sections
-
-    if args.year == 2021:
-        # 2021 exam next has 'Domain' section.
-        domain = get_block(r'^Domain: ', r'Citations: ')
-        log(f'{domain=}')
-    else:
-        domain = None
-
-    if args.year == 2021 or args.year == 2019:
-        # next comes 'Citations'. It can be in multiple blocks, with blank lines.
-        citations = get_block(r'^Citations? ?: ?', r'^Rationales? ?: ?')
-        log(f'{citations=}')
-
-        rationale = get_block(r'^Rationales?:', r'^\s?%s\. ' % (nextq+1) )
-        log(f'{rationale=}')
-
-    if args.year == 2020 or args.year == 2018:
-        rationale = get_block(r'^Rationales? ?: ?', r'^References? ?: ?')
-        log(f'{rationale=}')
-
-        # next comes 'References', also can be multiple blocks of line
-        # we will call them 'citations' to match what 2021 exam called this section
-        citations = get_block(r'^References? ?: ?', r'^\s?%s\. ' % (nextq+1) )
-        log(f'{citations=}')
-
-    if args.year == 2017 or args.year == 2016:
-        # Though this section is called 'Solution' in 2016 and 2017 tests
-        # we call it 'rationale' to match most other tests
-        rationale = get_block(r'^Solution ?: ?', r'^References? ?: ?')
-        log(f'{rationale=}')
-
-        # next comes 'References', also can be multiple blocks of line
-        # we will call them 'citations' to match what 2021 exam called this section
-        citations = get_block(r'^References? ?: ?', r'^\s?%s\. ' % (nextq+1) )
-        log(f'{citations=}')
-
+    key, domain, citations, rationale = [None] * 4   # we don't have these fields in this exam.
+    # we will get 'key' later, however.
     ques = Question(args.year, qnum, q, opts, key, domain, citations, rationale)
     if keep_question:
         print(ques)
